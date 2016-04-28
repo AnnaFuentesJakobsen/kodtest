45ba4c2f39aaf7609c43d235eae84d8485fad3ad
Anna Fuentes Jakobsen <afuentesjakobsen@Annas-MacBook-Pro-2.local>
2016-04-28 20:49:21 +0200

    added js file with gray boxes in gallery. also put in a add button

---
+++ agria.js
@@ -0,0 +1,43 @@
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "#b760a9";
+ctx.fillRect(0, 120,100, 30);
+
+
+
+var myTable = document.getElementById("myTable");
+
+
+function makeRowsAndColumns() {
+	for(var i = 1; i < 5; i++){
+
+		var addRow = document.createElement("TR");
+		console.log(addRow);
+
+			for(var j = 0; j < 7; j++){
+				var addCell = document.createElement("TD");
+				addCell.setAttribute("id", i + "" + j );
+				addCell.addEventListener("click", setColor);
+
+				addRow.appendChild(addCell);
+
+
+			}
+
+		document.getElementById("myTable").appendChild(addRow);
+	}
+
+}
+makeRowsAndColumns();
+
+function setColor(id){
+	//console.log(this.id);
+
+	for(var i = 0; i < rows.length; i++){
+
+	var rows = document.getElementsByTagName("TD");
+		rows.style.color = "grey";
+	}
+}

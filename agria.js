7a3f903123c076f26bb2186d8258dbd074fbadab
Anna Fuentes Jakobsen <afuentesjakobsen@Annas-MacBook-Pro-2.local>
2016-05-02 15:49:47 +0200

    fixed so code looked better and more readable with comments

--- agria.js
+++ agria.js
@@ -1,3 +1,7 @@
+/*
+ * Summary: Creates rows and columns with grey boxes.
+ */
+
 var myTable = document.getElementById("myTable");
 
 function makeRowsAndColumns() {
@@ -9,26 +13,10 @@ function makeRowsAndColumns() {
 			for(var j = 0; j < 7; j++){
 				var addCell = document.createElement("TD");
 				addCell.setAttribute("id", i + "" + j );
-				addCell.addEventListener("click", setColor);
 
 				addRow.appendChild(addCell);
-
-
 			}
-
 		document.getElementById("myTable").appendChild(addRow);
 	}
-
 }
 makeRowsAndColumns();
-
-function setColor(id){
-	//console.log(this.id);
-
-	for(var i = 0; i < rows.length; i++){
-
-	var rows = document.getElementsByTagName("TD");
-		rows.style.color = "grey";
-	}
-}
-
